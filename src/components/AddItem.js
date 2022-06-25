3cc748c1d80dabb0fcec86e1206de3279bfeb8a7
atharva030 <atharvajagdale28@gmail.com>
2022-06-25 17:29:14 +0530

    commit

---
+++ src/components/AddItem.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+
+class AddItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productName: "",
+      productPrice: "",
+    };
+  }
+  render() {
+    return (
+      <form
+        className="row mb-5"
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (!this.state.productName || !this.state.productPrice) {
+            alert("Please Fill the inputs");
+          } else {
+            this.props.addItem(this.state.productName, this.state.productPrice);
+          }
+        }}
+      >
+        <div className="mb-3 col-4">
+          <label htmlFor="inputName" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="name"
+            name="productName"
+            onChange={(e) => {
+              this.setState({ productName: e.currentTarget.value });
+            }}
+            value={this.state.productName}
+          />
+        </div>
+        <div className="mb-3 col-4">
+          <label htmlFor="inputPrice" className="form-label">
+            Price
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="pricerice"
+            name="productPrice"
+            onChange={(e) => {
+              this.setState({ productPrice: Number(e.currentTarget.value) });
+            }}
+            value={this.state.productPrice}
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="btn btn-primary col-1 mx-3 my-5"
+        >
+          Add
+        </button>
+        
+      </form>
+    );
+  }
+}
+
+export default AddItem;
